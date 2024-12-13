adbfc87b118d4beb9d9339f231fcd7c634c1581c
MedTaherGhazel <medtaherghazel@gmail.com>
2024-12-13 11:49:06 +0100

    first commit

---
+++ business_logic/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+app.use(express.static('../presentation'));
+
+const dataFilePath = path.join(__dirname, '../data/data.json');
+app.get('/api/message', (req, res) => {
+    fs.readFile(dataFilePath, 'utf-8', (err, data) => {
+        if (err) {
+            return res.status(500).json({ message: "Error reading JSON data" });
+        }
+        const jsonData = JSON.parse(data);
+        res.json({ message: jsonData.message });
+    });
+  
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
